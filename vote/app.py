7cb774e267381c32135aaa233a40ca6dbbc9492d
thutinski <tino.hutinski@gmail.com>
2024-12-09 10:44:18 +0100

    test github action v11

--- vote/app.py
+++ vote/app.py
@@ -8,7 +8,7 @@ import json
 option_a = os.getenv('OPTION_A', "CATS")
 option_b = os.getenv('OPTION_B', "DOGS")
 hostname = socket.gethostname()
-version = 'v153'
+version = 'v155'
 
 
 app = Flask(__name__)
